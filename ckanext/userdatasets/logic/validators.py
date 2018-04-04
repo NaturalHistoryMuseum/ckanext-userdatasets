0a38f4f9a643dc8cedea4e41ec4661ed60ddd13b
Alice Butcher <alycejenni@gmail.com>
2018-04-04 11:54:15 +0100

    manual changes to 'logic'

--- ckanext/userdatasets/logic/validators.py
+++ ckanext/userdatasets/logic/validators.py
@@ -4,11 +4,11 @@
 # This file is part of ckanext-userdatasets
 # Created by the Natural History Museum in London, UK
 
-import ckan.lib.navl.dictization_functions as df
 from ckan.new_authz import users_role_for_group_or_org
-from ckan.logic.validators import owner_org_validator as default_oov
 
-missing = df.missing
+from ckan.logic.validators import owner_org_validator as default_owner_org_validator
+from ckan.plugins import toolkit
+
 
 def owner_org_validator(key, data, errors, context):
     '''
@@ -21,9 +21,9 @@ def owner_org_validator(key, data, errors, context):
     '''
     owner_org = data.get(key)
     user = context[u'auth_user_obj']
-    if owner_org is not missing and owner_org is not None and owner_org != u'':
+    if owner_org is not toolkit.missing and owner_org is not None and owner_org != u'':
         role = users_role_for_group_or_org(owner_org, user.name)
         if role == u'member':
             return
 
-    default_oov(key, data, errors, context)
+    default_owner_org_validator(key, data, errors, context)
