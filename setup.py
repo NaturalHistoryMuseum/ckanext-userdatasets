14b536cdf3105195b3486ef727fdf73992cb9426
Alice Butcher <alycejenni@gmail.com>
2019-07-23 18:38:30 +0100

    Standardise extension documentation/metadata

--- setup.py
+++ setup.py
@@ -4,22 +4,39 @@
 # This file is part of ckanext-userdatasets
 # Created by the Natural History Museum in London, UK
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-version = u'0.1'
+__version__ = u'1.0.0-alpha'
+
+with open(u'README.md', u'r') as f:
+    __long_description__ = f.read()
 
 setup(
-	name=u'ckanext-userdatasets',
-	version=version,
-	description=u'CKAN extension to allow users to edit their dataset only',
+    name=u'ckanext-userdatasets',
+    version=__version__,
+    description=u'A CKAN extension that allows organisation members to create datasets, and edit or delete the datasets they have created.',
+    long_description=__long_description__,
+    classifiers=[
+        u'Development Status :: 3 - Alpha',
+        u'Framework :: Flask',
+        u'Programming Language :: Python :: 2.7'
+    ],
+    keywords=u'CKAN data userdatasets',
+    author=u'Natural History Museum',
+    author_email=u'data@nhm.ac.uk',
     url=u'https://github.com/NaturalHistoryMuseum/ckanext-userdatasets',
-	packages=find_packages(),
-	namespace_packages=[u'ckanext', u'ckanext.userdatasets'],
-	install_requires=[
+    license=u'GNU GPLv3',
+    packages=find_packages(exclude=[u'tests']),
+    namespace_packages=[u'ckanext', u'ckanext.userdatasets'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
 		u'importlib',
-	],	
-	entry_points=u'''
+	],
+    entry_points= \
+        u'''
         [ckan.plugins]
-            userdatasets = ckanext.userdatasets.plugin:UserDatasetsPlugin
-	''',
-)
+            userdatasets=ckanext.userdatasets.plugin:UserDatasetsPlugin
+
+        ''',
+    )
