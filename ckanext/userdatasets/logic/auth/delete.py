3a31e3b6901108fd8be1c9885ebb17cf02af38b1
Ginger Butcher <alycejenni@gmail.com>
2022-11-15 12:29:54 +0000

    style: apply formatting

--- ckanext/userdatasets/logic/auth/delete.py
+++ ckanext/userdatasets/logic/auth/delete.py
@@ -7,9 +7,11 @@
 from ckan.logic.auth import get_package_object, get_resource_object
 from ckan.plugins import toolkit
 
-from ckanext.userdatasets.logic.auth.auth import (get_resource_view_object,
-                                                  user_is_member_of_package_org,
-                                                  user_owns_package_as_member)
+from ckanext.userdatasets.logic.auth.auth import (
+    get_resource_view_object,
+    user_is_member_of_package_org,
+    user_owns_package_as_member,
+)
 
 
 @toolkit.chained_auth_function
@@ -24,9 +26,7 @@ def package_delete(next_auth, context, data_dict):
     package = get_package_object(context, data_dict)
 
     if user_owns_package_as_member(user, package):
-        return {
-            'success': True
-        }
+        return {'success': True}
 
     return next_auth(context, data_dict)
 
@@ -43,13 +43,9 @@ def resource_delete(next_auth, context, data_dict):
     resource = get_resource_object(context, data_dict)
     package = resource.package
     if user_owns_package_as_member(user, package):
-        return {
-            'success': True
-        }
+        return {'success': True}
     elif user_is_member_of_package_org(user, package):
-        return {
-            'success': False
-        }
+        return {'success': False}
 
     return next_auth(context, data_dict)
 
@@ -64,16 +60,10 @@ def resource_view_delete(next_auth, context, data_dict):
     '''
     user = context['auth_user_obj']
     resource_view = get_resource_view_object(context, data_dict)
-    resource = get_resource_object(context, {
-        'id': resource_view.resource_id
-    })
+    resource = get_resource_object(context, {'id': resource_view.resource_id})
     if user_owns_package_as_member(user, resource.package):
-        return {
-            'success': True
-        }
+        return {'success': True}
     elif user_is_member_of_package_org(user, resource.package):
-        return {
-            'success': False
-        }
+        return {'success': False}
 
     return next_auth(context, data_dict)
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -28,48 +28,58 @@ def org():
 @pytest.fixture
 def member_1(org):
     user = factories.User(name='test_member_1')
-    call_action('organization_member_create', id=org['id'], username=user['name'],
-                role='member')
+    call_action(
+        'organization_member_create', id=org['id'], username=user['name'], role='member'
+    )
     return user
 
 
 @pytest.fixture
 def member_2(org):
     user = factories.User(name='test_member_2')
-    call_action('organization_member_create', id=org['id'], username=user['name'],
-                role='member')
+    call_action(
+        'organization_member_create', id=org['id'], username=user['name'], role='member'
+    )
     return user
 
 
 @pytest.fixture
 def editor(org):
     user = factories.User(name='test_editor')
-    call_action('organization_member_create', id=org['id'], username=user['name'],
-                role='editor')
+    call_action(
+        'organization_member_create', id=org['id'], username=user['name'], role='editor'
+    )
     return user
 
 
 @pytest.mark.ckan_config('ckan.plugins', 'userdatasets')
 @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
 class TestFuncDatasets(object):
-
     def test_members_can_edit_own_datasets(self, org, member_1):
-        '''Tests datasets'''
+        """
+        Tests datasets.
+        """
         package = create_package(org, member_1)
         context = {'user': member_1['name']}
-        package = toolkit.get_action('package_update')(context, {
-            'id': package['id'],
-            'title': 'title for test_members_can_edit_own_datasets'
-        })
+        package = toolkit.get_action('package_update')(
+            context,
+            {
+                'id': package['id'],
+                'title': 'title for test_members_can_edit_own_datasets',
+            },
+        )
         assert package['title'] == 'title for test_members_can_edit_own_datasets'
 
     def test_editors_can_edit_org_datasets(self, org, member_1, editor):
         package = create_package(org, member_1)
         context = {'user': editor['name']}
-        package = toolkit.get_action('package_update')(context, {
-            'id': package['id'],
-            'title': 'title for test_editors_can_edit_org_datasets'
-        })
+        package = toolkit.get_action('package_update')(
+            context,
+            {
+                'id': package['id'],
+                'title': 'title for test_editors_can_edit_org_datasets',
+            },
+        )
         assert package['title'] == 'title for test_editors_can_edit_org_datasets'
 
     def test_members_cannot_edit_others_datasets(self, org, member_1, member_2):
@@ -77,38 +87,38 @@ class TestFuncDatasets(object):
         # Attempt to update test_pkg_1 as member test_member_2
         with pytest.raises(toolkit.NotAuthorized, match=member_2['name']):
             context = {'user': member_2['name']}
-            toolkit.get_action('package_update')(context, {
-                'id': package['id'],
-                'title': 'title for test_members_cannot_edit_others_datasets'
-            })
+            toolkit.get_action('package_update')(
+                context,
+                {
+                    'id': package['id'],
+                    'title': 'title for test_members_cannot_edit_others_datasets',
+                },
+            )
 
         # Attempt to delete test_pkg_1 as member test_member_2
         with pytest.raises(toolkit.NotAuthorized, match=member_2['name']):
             context = {'user': member_2['name']}
-            toolkit.get_action('package_delete')(context, {
-                'id': package['id']
-            })
+            toolkit.get_action('package_delete')(context, {'id': package['id']})
 
     def test_non_members_cannot_create_datasets(self, member_1):
         with pytest.raises(toolkit.NotAuthorized, match=member_1['name']):
             another_org = factories.Organization()
-            toolkit.get_action('package_create')({'user': member_1['name']},
-                                                  {'owner_org': another_org['id']})
+            toolkit.get_action('package_create')(
+                {'user': member_1['name']}, {'owner_org': another_org['id']}
+            )
 
 
 @pytest.mark.ckan_config('ckan.plugins', 'userdatasets')
 @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
 class TestFuncResources(object):
-
     def test_members_can_edit_own_resources(self, org, member_1):
         package = create_package(org, member_1)
         resource = factories.Resource(package_id=package['id'])
 
         context = {'user': member_1['name']}
-        resource = toolkit.get_action('resource_update')(context, {
-            'id': resource['id'],
-            'description': 'test description 2'
-        })
+        resource = toolkit.get_action('resource_update')(
+            context, {'id': resource['id'], 'description': 'test description 2'}
+        )
         assert resource['description'] == 'test description 2'
 
     def test_editors_can_edit_org_resources(self, org, member_1, editor):
@@ -116,10 +126,10 @@ class TestFuncResources(object):
         resource = factories.Resource(package_id=package['id'])
 
         context = {'user': editor['name']}
-        resource = toolkit.get_action('resource_update')(context, {
-            'id': resource['id'],
-            'description': 'test description for test_editor'
-        })
+        resource = toolkit.get_action('resource_update')(
+            context,
+            {'id': resource['id'], 'description': 'test description for test_editor'},
+        )
         assert resource['description'] == 'test description for test_editor'
 
     def test_members_cannot_edit_others_resources(self, org, member_1, member_2):
@@ -129,24 +139,23 @@ class TestFuncResources(object):
         context = {'user': member_2['name']}
 
         with pytest.raises(toolkit.NotAuthorized):
-            resource = toolkit.get_action('resource_update')(context, {
-                'id': resource['id'],
-                'description': 'this should not work'
-            })
+            resource = toolkit.get_action('resource_update')(
+                context, {'id': resource['id'], 'description': 'this should not work'}
+            )
 
         with pytest.raises(toolkit.NotAuthorized):
             toolkit.get_action('resource_delete')(context, {'id': resource['id']})
 
         with pytest.raises(toolkit.NotAuthorized):
-            toolkit.get_action('resource_create')(context, {'package_id': package['id'],
-                                                             'description': 'beans!'})
+            toolkit.get_action('resource_create')(
+                context, {'package_id': package['id'], 'description': 'beans!'}
+            )
 
 
 # the ResourceView factory requires the use of the image_view plugin for default functionality
 @pytest.mark.ckan_config('ckan.plugins', 'userdatasets image_view')
 @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
 class TestFuncResourceViews(object):
-
     def test_members_can_edit_own_resource_views(self, org, member_1):
         package = create_package(org, member_1)
         resource = factories.Resource(package_id=package['id'])
@@ -154,11 +163,14 @@ class TestFuncResourceViews(object):
 
         context = {'user': member_1['name']}
         new_desc = 'description for test_members_can_edit_own_resource_views'
-        resource_view_updated = toolkit.get_action('resource_view_update')(context, {
-            'id': resource_view['id'],
-            'resource_id': resource['id'],
-            'description': new_desc
-        })
+        resource_view_updated = toolkit.get_action('resource_view_update')(
+            context,
+            {
+                'id': resource_view['id'],
+                'resource_id': resource['id'],
+                'description': new_desc,
+            },
+        )
         assert resource_view_updated['description'] == new_desc
         assert resource_view['description'] != resource_view_updated['description']
 
@@ -169,11 +181,14 @@ class TestFuncResourceViews(object):
 
         context = {'user': editor['name']}
         new_desc = 'description for test_editors_can_edit_org_resource_views'
-        resource_view_updated = toolkit.get_action('resource_view_update')(context, {
-            'id': resource_view['id'],
-            'resource_id': resource['id'],
-            'description': new_desc
-        })
+        resource_view_updated = toolkit.get_action('resource_view_update')(
+            context,
+            {
+                'id': resource_view['id'],
+                'resource_id': resource['id'],
+                'description': new_desc,
+            },
+        )
         assert resource_view_updated['description'] == new_desc
         assert resource_view['description'] != resource_view_updated['description']
 
@@ -186,21 +201,26 @@ class TestFuncResourceViews(object):
         new_desc = 'description for test_members_cannot_edit_others_resource_views'
 
         with pytest.raises(toolkit.NotAuthorized):
-            toolkit.get_action('resource_view_update')(context, {
-                'id': resource_view['id'],
-                'resource_id': resource['id'],
-                'description': new_desc
-            })
+            toolkit.get_action('resource_view_update')(
+                context,
+                {
+                    'id': resource_view['id'],
+                    'resource_id': resource['id'],
+                    'description': new_desc,
+                },
+            )
 
         with pytest.raises(toolkit.NotAuthorized):
-            toolkit.get_action('resource_view_delete')(context, {
-                'id': resource_view['id'],
-                'resource_id': resource['id']
-            })
+            toolkit.get_action('resource_view_delete')(
+                context, {'id': resource_view['id'], 'resource_id': resource['id']}
+            )
 
         with pytest.raises(toolkit.NotAuthorized):
-            toolkit.get_action('resource_view_create')(context, {
-                'resource_id': resource['id'],
-                'title': 'beans!',
-                'view_type': 'image_view'
-            })
+            toolkit.get_action('resource_view_create')(
+                context,
+                {
+                    'resource_id': resource['id'],
+                    'title': 'beans!',
+                    'view_type': 'image_view',
+                },
+            )
